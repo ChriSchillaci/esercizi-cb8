45f4e10db52cd7dc286cc32e2c20400e0adb8c84
ChriSchillaci <chri2001@outlook.com>
2023-11-01 11:45:52 +0100

    added map method

--- 1-11-23/app.js
+++ 1-11-23/app.js
@@ -1,5 +1,22 @@
-const animals = ['Tiger', 'Lion', 'Dog', 'Cat', 'Wolf', 'Hawk', 'Seagull', 'Hedgehog']
+const animals = [
+  "Tiger",
+  "Lion",
+  "Dog",
+  "Cat",
+  "Wolf",
+  "Hawk",
+  "Seagull",
+  "Hedgehog",
+];
+
+console.log("CICLO FOR");
 
 for (let i = 0; i < animals.length; i++) {
-    console.log(i + 1 + ': ' + animals[i])
+  console.log(i + 1 + ": " + animals[i]);
 }
+console.log("");
+console.log("METODO ARRAY 'MAP'");
+
+const animalsMap = animals.map((item, index) => {
+  console.log(`${index + 1}: ${item}`);
+});
